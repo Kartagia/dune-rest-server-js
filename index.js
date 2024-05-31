de1f31e664bf30b51d691a902812e00551a4acdf
Antti Kautiainen <kautsu@elisanet.fi>
2024-05-31 12:41:13 +0300

    feat: people rest comamnds, but patch

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+
+
 const express = require("express");
 const app = express();
 const logger = console;
+
+app.use(express.json());
 /**
  * The port of the application.
  */
 const PORT = 3000;
 
 const {defaultMenu} = require("./lib/menu.js")
+const {dummyDao } = require("./lib/people.js")
 
 
 /**
@@ -128,6 +133,78 @@ app.get("/", mainIndexPageHandler);
 app.get("/data/", dataResourceHandler);
 app.get("/menu/", menuResourceHandler);
 
+// People
+
+// getAll:People 
+app.get("/people/", (req, res) => {
+  const id = req.params.id;
+  console.log(`Handling request: get all people`);
+  dummyDao.all().then( (result) => {
+    res.json(result);
+  }, (error) => {
+    console.log(`Fetching all people failed due error ${error}`);
+    res.sendStatus(401);
+  });
+});
+
+
+// get:Poeple
+app.get("/people/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`Handling request: get people "${id}"`);
+  dummyDao.one(id).then( (result) => {
+    console.log(`People "${id}" found - sending ${JSON.stringify(result)}`);
+    res.json(result);
+  }, (error) => {
+    console.log(`People "${id}" not found`);
+    res.sendStatus(404);
+  });
+});
+
+// delete:People
+app.delete("/people/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`Handling request: delete people "${id}"`);
+  dummyDao.remove(id).then( (result) => {
+    console.log(`People "${id}" ${result ? "": "not"} deleted - sending ${JSON.stringify(result)}`);
+    res.sendStatus(result ? 204 : 404);
+  }, (error) => {
+    console.log(`Delete failed due error ${error}`);
+    res.sendStatus(404);
+  });
+});
+
+
+// create:People 
+app.post("/people", (req, res) => {
+  const body = req.body;
+  console.log(`Handling request: create people: ${body}`);
+  dummyDao.create(body).then( (result) => {
+    console.log(`Created People with Id ${JSON.stringify(result)}`);
+    res.json(result);
+  }, (error) => {
+    console.log(`People "${id}" not found`);
+    res.sendStatus(404);
+  });
+});
+
+
+// update:People 
+app.put("/people/:id", (req, res) => {
+  const id = req.params.id;
+  const added = req.body;
+  console.log(`Handling request: update people "${id}" to ${added}`);
+  dummyDao.update(id, added).then( (result) => {
+    console.log(`People "${id}" ${result ? "updated":"not updated"}`);
+    res.sendStatus(result ? 200 : 400 );
+  }, (error) => {
+    console.log(`Update failed due error ${error}`);
+    res.sendStatus(400);
+  });
+});
+
+
+// Starting the server.
 app.listen(PORT, () => {
   logger.log(`Dune Rest server running on ${PORT}`);
 });
