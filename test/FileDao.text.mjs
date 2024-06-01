e071de925c8831f595e24bf86a357bd8b1ede3c0
Antti Kautiainen <kautsu@elisanet.fi>
2024-06-01 11:08:22 +0300

    test: FileDao with valueValidator modified:   test/FileDao.text.mjs - test: Test case FIleDao with ValueValidator

--- test/FileDao.text.mjs
+++ test/FileDao.text.mjs
@@ -57,6 +57,68 @@ describe("Class File Dao", function () {
         it("create", function() {
             expect(tested.create("zero")).eventually.rejectedWith(TypeError);
         });
+        it("delete", function() {
+            expect(tested.delete(1)).eventually.equal(true);
+            expect(tested.delete(1)).eventually.equal(false);
+            expect(tested.one(1)).eventually.rejectedWith(RangeError);
+        });
+    });
+
+
+    describe("FileDao<number,string> with value validator for values ['zero', 'one', 'two', 'three', 'four']", function() {
+        let tested;
+
+        function getId(/** @type {string} */ value) {
+            if (typeof value === "string") {
+                switch (value.toLowerCase().normalize()) {
+                    case "zero": return 0;
+                    case "one": return 1;
+                    case "two": return 2;
+                    case "three": return 3;
+                    case "four": return 4;
+                    default:
+                        throw new TypeError("Invalid value");
+                }
+            } else {
+                throw new TypeError("Invalid value");
+            }
+        }
+        const valueValidator = ( /** @type {string} */ value, /** @type {number} */ id=undefined) => {
+            const valueId = getId(value);
+            if (id !== undefined && id !== valueId) {
+                throw new RangeError("The given identifier is invalid");
+            }
+            return [valueId, value];
+        }
+        it("Construction", function() {
+            expect( () => {tested = new FileDao("number_string.dao.json", undefined, undefined, valueValidator)}).not.throw();
+            expect(tested).not.null;
+            expect(tested.all()).eventually.property("length", 3);
+        });
+        it("all", function() {
+            expect(tested.all()).eventually.deep.equal([[1, "one"], [2, "two"], [3, "three"]]);
+        });
+        it("one", function() {
+            expect(tested.one(1)).eventually.equal("one");
+            expect(tested.one(2)).eventually.equal("two");
+            expect(tested.one(3)).eventually.equal("three");
+            expect(tested.one(4)).eventually.rejectedWith(RangeError);
+        });
+        it("update", function() {
+            expect(tested.update(1, "One")).eventually.equal(true);
+            expect(tested.one(1)).eventually.equal("One");
+            expect(tested.update(0, "Zero")).eventually.rejectedWith(RangeError);
+        });
+        it("create", function() {
+            expect(tested.create("zero")).eventually.equal(0);
+            expect(tested.create("Four")).eventually.equal(4);
+            expect(tested.create("five")).eventually.rejectedWith(TypeError);
+        });
+        it("delete", function() {
+            expect(tested.delete(1)).eventually.equal(true);
+            expect(tested.delete(1)).eventually.equal(false);
+            expect(tested.one(1)).eventually.rejectedWith(RangeError);
+        });
     });
 
 });
