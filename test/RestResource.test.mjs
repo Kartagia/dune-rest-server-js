dffdfb43267c0e2c31343694064f662b64a97c0b
Antti Kautiainen <antti@kautiainen.com>
2024-06-03 12:14:29 +0300

    test: class RestResources unit testing new file:   test/RestResource.test.mjs - test: RestResource.checkParseId

---
+++ test/RestResource.test.mjs
@@ -0,0 +1,93 @@
+import { describe, it } from "mocha";
+import chaiAsPromised from "chai-as-promised";
+import { use, expect } from "chai";
+import { BasicDao } from "../src/BasicDao.mjs";
+import { RestResource, orElse } from "../src/restServer.mjs";
+
+describe("class RestResource", function () {
+  /**
+   * The basic dao.
+   * @type {BasicDao<number, string>}
+   */
+  [
+    ["Readonly empty BasicDao<number,string>", new BasicDao({}), []],
+    [],
+  ].forEach(
+    ([
+      testName,
+      testDao,
+      daoEntries,
+      genId = undefined,
+      formatId = undefined,
+      parseId = undefined,
+      formatData = undefined,
+      parseData = undefined,
+      linkGenerator = undefined,
+    ]) => {
+      /**
+       * @type {(import("../src/restServer.mjs").RestResourceOptions<number,string> & {
+       * caseName:string})[]}
+       */
+      const testValues = [
+        {
+          caseName: "undefined",
+          parseId: undefined,
+          formatId: undefined,
+          parseValue: undefined,
+          formatValue: undefined,
+        },
+        {
+          caseName: "JSON",
+          parseId: (val) => {
+            const num = JSON.parse(num);
+            if (Number.isInteger(num)) {
+              return num;
+            } else {
+              throw new RangeError("Invalid identifief");
+            }
+          },
+          formatId: (id) => JSON.format(id),
+          formatValue: (val) => JSON.stringify(val),
+          parseValue: (val) => JSON.parse(val),
+        },
+      ];
+
+      describe(`Test ${testName}:`, function () {
+        describe("RestResource<number, string>.checkParseId", function () {
+          testValues
+            .map((options) => {
+              return [options.caseName, options.parseId, orElse(options.parseId, RestResource.defaultIdParser)]
+            })
+            .forEach(
+              ([
+                caseName,
+                testValue,
+                expectedResult = undefined,
+                expectedException = undefined
+              ]) => {
+                it(`Test ${caseName}`, function () {
+                  if (expectedException !== undefined) {
+                    expect(() => {
+                      RestResource.checkIdFormatter(testValue);
+                    }).to.throw(expectedException);
+                  } else {
+                    expect(() => {
+                      RestResource.checkIdFormatter(testValue);
+                    }).to.not.throw();
+                    const result = RestResource.checkIdFormatter(testValue);
+                    expect(result).equal(expectedResult);
+                  }
+                });
+              }
+            );
+        });
+      });
+
+      describe("RestResource<number, string>.checkFormatId", function () {});
+
+      describe("RestResource<number, string>.checkFormatValue", function () {});
+
+      describe("RestResource<number, string>.checkParseValue", function () {});
+    }
+  );
+});
