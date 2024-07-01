b5aefc8e8a3b4bd6b09e2e4057d41ecf2224691a
Antti Kautiainen <antti@kautiainen.com>
2024-07-01 12:11:38 +0300

    fix: webpack config for people

--- webpack.compile.config.js
+++ webpack.compile.config.js
@@ -4,8 +4,21 @@
  */
 const path = require('path');
 
-module.exports = [{
-    entry: {menu: "./src/[name].mjs", people: "./src/[name].mjs", dao: "./src/BasicDao.mjs"},
+module.exports = [
+{
+    entry: {people: "./src/people.mjs"},
+    output: {
+        path: path.resolve(__dirname, "lib"),
+        filename: '[name].js',
+        globalObject: 'this',
+        library:  {
+            name: '[name]',
+            type: 'umd'
+        }
+    }
+},
+{
+    entry: {dao: "./src/BasicDao.mjs"},
     output: {
         path: path.resolve(__dirname, "lib"),
         filename: '[name].js',
